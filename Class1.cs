9c25ba53440d49160fe63c959f6407dea4082f74
raphael farache <raphael.farache@outlook.fr>
2020-12-03 14:14:14 +0200

    jai mis test1fghj

---
+++ Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_00_3224_5675
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome3224();
+            Welcome5675();
+            Console.ReadKey();
+        }
+
+        static partial void Welcome5675();
+
+        private static void Welcome3224()
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to first console application ", name);
+        }
+    }
+}
